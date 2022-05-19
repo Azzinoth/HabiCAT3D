47fce9db2074d4e5e778cf62c996e34083c0721a
Azzinoth <kandrat2007@yandex.ru>
2022-05-19 11:22:50 -0400

    glm support added

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include "FEBasicApplication/FEBasicApplication.h"
 using namespace FocalEngine;
 
+#define GLM_FORCE_XYZW_ONLY
+#include "glm/vec3.hpp"
+#include "glm/vec4.hpp"
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.inl"
+#include "glm/gtx/quaternion.hpp"
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	APPLICATION.createWindow(1280, 720, "Rugosity Calculator");
+
+	glm::vec3 testVector = glm::vec3(0.0);
 
 	while (APPLICATION.isWindowOpened())
 	{
