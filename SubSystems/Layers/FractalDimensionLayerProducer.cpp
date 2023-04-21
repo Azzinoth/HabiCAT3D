baf7031972fe57b9054cc69b9427f6b5f6a659dc
Azzinoth <kindratberegovyi@gmail.com>
2023-04-20 22:25:14 -0400

    WIP Fractal Dimension

--- SubSystems/Layers/FractalDimensionLayerProducer.cpp
+++ SubSystems/Layers/FractalDimensionLayerProducer.cpp
@@ -39,46 +39,63 @@ std::vector<double> generateBoxSizes(double minSize, double maxSize, double fact
 	return sizes;
 }
 
-double calculateFractalDimension(const FEAABB& BoundingBox, const std::vector<Triangle>& triangles)
+void FractalDimensionLayerProducer::WorkOnNode(SDFNode* CurrentNode)
 {
+	if (CurrentNode->TrianglesInCell.empty())
+		return;
+
 	// Generate a sequence of box sizes
-	std::vector<double> boxSizes = generateBoxSizes(1.0 / 128, 1.0, 2);
+	std::vector<double> boxSizes = generateBoxSizes(1.0 / 256, 0.5, 2);
 
 	std::vector<double> logInverseSizes;
 	std::vector<double> logCounts;
 
-	for (size_t i = 0; i < boxSizes.size(); ++i) {
+	for (size_t i = 0; i < boxSizes.size(); i++)
+	{
 		double boxSize = boxSizes[i];
 
 		// Create a 3D grid that covers the entire bounding box
-		int gridX = static_cast<int>(glm::ceil((Boundingbox.Get Max.X - bbox.minX) / boxSize));
-		int gridY = static_cast<int>(glm::ceil((bbox.maxY - bbox.minY) / boxSize));
-		int gridZ = static_cast<int>(glm::ceil((bbox.maxZ - bbox.minZ) / boxSize));
+		int gridX = static_cast<int>(glm::ceil((CurrentNode->AABB.getMax()[0] - CurrentNode->AABB.getMin()[0]) / boxSize));
+		int gridY = static_cast<int>(glm::ceil((CurrentNode->AABB.getMax()[1] - CurrentNode->AABB.getMin()[1]) / boxSize));
+		int gridZ = static_cast<int>(glm::ceil((CurrentNode->AABB.getMax()[2] - CurrentNode->AABB.getMin()[2]) / boxSize));
 
 		int count = 0;
 		std::vector<std::vector<std::vector<bool>>> grid(gridX, std::vector<std::vector<bool>>(gridY, std::vector<bool>(gridZ, false)));
 
 		// Iterate through all the triangles
-		for (const Triangle& triangle : triangles) {
-			// Calculate the grid cells that the triangle intersects or is contained in
-			BoundingBox triangleBBox = triangle.boundingBox();
-			int minGridX = static_cast<int>((triangleBBox.minX - bbox.minX) / boxSize);
-			int minGridY = static_cast<int>((triangleBBox.minY - bbox.minY) / boxSize);
-			int minGridZ = static_cast<int>((triangleBBox.minZ - bbox.minZ) / boxSize);
-			int maxGridX = static_cast<int>((triangleBBox.maxX - bbox.minX) / boxSize);
-			int maxGridY = static_cast<int>((triangleBBox.maxY - bbox.minY) / boxSize);
-			int maxGridZ = static_cast<int>((triangleBBox.maxZ - bbox.minZ) / boxSize);
+		for (size_t j = 0; j < CurrentNode->TrianglesInCell.size(); j++)
+		{
+			std::vector<glm::vec3> CurrentTriangle = MESH_MANAGER.ActiveMesh->Triangles[CurrentNode->TrianglesInCell[j]];
 
-			for (int x = minGridX; x <= maxGridX; ++x) {
-				for (int y = minGridY; y <= maxGridY; ++y) {
-					for (int z = minGridZ; z <= maxGridZ; ++z) {
-						if (!grid[x][y][z]) {
-							glm::vec3 boxMin(x * boxSize + bbox.minX, y * boxSize + bbox.minY, z * boxSize + bbox.minZ);
-							glm::vec3 boxMax((x + 1) * boxSize + bbox.minX, (y + 1) * boxSize + bbox.minY, (z + 1) * boxSize + bbox.minZ);
-							BoundingBox box(boxMin, boxMax);
-							if (intersect(triangle, box)) {
-								grid[x][y][z] = true;
-								++count;
+			// Calculate the grid cells that the triangle intersects or is contained in
+			FEAABB TriangleBBox = FEAABB(CurrentTriangle);
+			int minGridX = static_cast<int>((TriangleBBox.getMin()[0] - CurrentNode->AABB.getMin()[0]) / boxSize);
+			int minGridY = static_cast<int>((TriangleBBox.getMin()[1] - CurrentNode->AABB.getMin()[1]) / boxSize);
+			int minGridZ = static_cast<int>((TriangleBBox.getMin()[2] - CurrentNode->AABB.getMin()[2]) / boxSize);
+			int maxGridX = static_cast<int>((TriangleBBox.getMax()[0] - CurrentNode->AABB.getMin()[0]) / boxSize);
+			int maxGridY = static_cast<int>((TriangleBBox.getMax()[1] - CurrentNode->AABB.getMin()[1]) / boxSize);
+			int maxGridZ = static_cast<int>((TriangleBBox.getMax()[2] - CurrentNode->AABB.getMin()[2]) / boxSize);
+
+			for (int x = minGridX; x <= maxGridX; ++x)
+			{
+				for (int y = minGridY; y <= maxGridY; ++y)
+				{
+					for (int z = minGridZ; z <= maxGridZ; ++z)
+					{
+						if (x >= 0 && x < gridX && y >= 0 && y < gridY && z >= 0 && z < gridZ)
+						{
+							if (!grid[x][y][z])
+							{
+								glm::vec3 boxMin(x * boxSize + CurrentNode->AABB.getMin()[0], y * boxSize + CurrentNode->AABB.getMin()[1], z * boxSize + CurrentNode->AABB.getMin()[2]);
+								glm::vec3 boxMax((x + 1) * boxSize + CurrentNode->AABB.getMin()[0], (y + 1) * boxSize + CurrentNode->AABB.getMin()[1], (z + 1) * boxSize + CurrentNode->AABB.getMin()[2]);
+								FEAABB box(boxMin, boxMax);
+
+								//if (intersect(triangle, box))
+								if (box.AABBIntersect(TriangleBBox))
+								{
+									grid[x][y][z] = true;
+									++count;
+								}
 							}
 						}
 					}
@@ -93,43 +110,37 @@ double calculateFractalDimension(const FEAABB& BoundingBox, const std::vector<Tr
 
 	// Perform linear regression to estimate the fractal dimension
 	std::pair<double, double> coefficients = linearRegression(logInverseSizes, logCounts);
-	double fractalDimension = coefficients.first;
+	double FractalDimension = coefficients.first;
 
-	return fractalDimension;
-}
+	CurrentNode->UserData = FractalDimension;
 
-void FractalDimensionLayerProducer::WorkOnNode(SDFNode* CurrentNode)
-{
-	if (CurrentNode->TrianglesInCell.empty())
-		return;
+	//std::vector<double> NormalX;
+	//std::vector<double> NormalY;
+	//std::vector<double> NormalZ;
 
-	std::vector<double> NormalX;
-	std::vector<double> NormalY;
-	std::vector<double> NormalZ;
+	//for (size_t p = 0; p < CurrentNode->TrianglesInCell.size(); p++)
+	//{
+	//	std::vector<glm::vec3> CurrentTriangleNormals = MESH_MANAGER.ActiveMesh->TrianglesNormals[CurrentNode->TrianglesInCell[p]];
 
-	for (size_t p = 0; p < CurrentNode->TrianglesInCell.size(); p++)
-	{
-		std::vector<glm::vec3> CurrentTriangleNormals = MESH_MANAGER.ActiveMesh->TrianglesNormals[CurrentNode->TrianglesInCell[p]];
+	//	for (size_t l = 0; l < CurrentTriangleNormals.size(); l++)
+	//	{
+	//		NormalX.push_back(CurrentTriangleNormals[l][0]);
+	//		NormalY.push_back(CurrentTriangleNormals[l][1]);
+	//		NormalZ.push_back(CurrentTriangleNormals[l][2]);
+	//	}
+	//}
 
-		for (size_t l = 0; l < CurrentTriangleNormals.size(); l++)
-		{
-			NormalX.push_back(CurrentTriangleNormals[l][0]);
-			NormalY.push_back(CurrentTriangleNormals[l][1]);
-			NormalZ.push_back(CurrentTriangleNormals[l][2]);
-		}
-	}
+	//double meanX = std::accumulate(NormalX.begin(), NormalX.end(), 0.0) / NormalX.size();
+	//double meanY = std::accumulate(NormalY.begin(), NormalY.end(), 0.0) / NormalY.size();
+	//double meanZ = std::accumulate(NormalZ.begin(), NormalZ.end(), 0.0) / NormalZ.size();
 
-	double meanX = std::accumulate(NormalX.begin(), NormalX.end(), 0.0) / NormalX.size();
-	double meanY = std::accumulate(NormalY.begin(), NormalY.end(), 0.0) / NormalY.size();
-	double meanZ = std::accumulate(NormalZ.begin(), NormalZ.end(), 0.0) / NormalZ.size();
+	//double sumX = std::inner_product(NormalX.begin(), NormalX.end(), NormalX.begin(), 0.0);
+	//double sumY = std::inner_product(NormalY.begin(), NormalY.end(), NormalY.begin(), 0.0);
+	//double sumZ = std::inner_product(NormalZ.begin(), NormalZ.end(), NormalZ.begin(), 0.0);
 
-	double sumX = std::inner_product(NormalX.begin(), NormalX.end(), NormalX.begin(), 0.0);
-	double sumY = std::inner_product(NormalY.begin(), NormalY.end(), NormalY.begin(), 0.0);
-	double sumZ = std::inner_product(NormalZ.begin(), NormalZ.end(), NormalZ.begin(), 0.0);
+	//double DoubleResult = sqrt(sumX / NormalX.size() - meanX * meanX + sumY / NormalY.size() - meanY * meanY + sumZ / NormalZ.size() - meanZ * meanZ);
 
-	double DoubleResult = sqrt(sumX / NormalX.size() - meanX * meanX + sumY / NormalY.size() - meanY * meanY + sumZ / NormalZ.size() - meanZ * meanZ);
-
-	CurrentNode->UserData = DoubleResult;
+	//CurrentNode->UserData = DoubleResult;
 }
 
 void FractalDimensionLayerProducer::CalculateWithJitterAsync(FEMesh* Mesh, bool bSmootherResult)
